51b0e7fb67a63a7d082c48de9ebfc0ebd7d95157
Luke Campbell <luke.s.campbell@gmail.com>
2016-06-16 11:45:00 -0400

    Adds error message to setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+import sys
+
+__version__ = '1.0.0'
+
+
+sys.stderr.write("This project is not packaged as a standard python package and can not be"
+                 " installed using setup.py or pip. Please consult the README for more information\n")
