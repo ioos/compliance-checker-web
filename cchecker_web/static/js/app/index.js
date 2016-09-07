72ffedc1baff9b591dc6e2b6e9d7048754212818
Bob Fratantonio <bobfrat@gmail.com>
2016-09-07 14:13:35 -0400

    Add description and references

--- cchecker_web/static/js/app/index.js
+++ cchecker_web/static/js/app/index.js
@@ -19,9 +19,37 @@ _.extend(App.prototype, {
   form: new FormData(),
   initializeViews: function() {
     var self = this;
+    var referenceUrls = _.compact([
+        {
+          href:  'http://wiki.esipfed.org/index.php/Category:Attribute_Conventions_Dataset_Discovery',
+          title: 'ACDD wiki',
+          text:  'ACDD',
+        },
+        {
+          href:  'http://cfconventions.org/documents.html',
+          title: 'CF Conventions',
+          text:  'CF Conventions',
+        },
+        {
+          href:  'https://github.com/ioos/ioosngdac/wiki/NGDAC-NetCDF-File-Format-Version-2',
+          title: 'NGDAC NetCDF File Format Version 2',
+          text:  'Glider DAC wiki',
+        },
+        {
+          href:  'https://www.nodc.noaa.gov/data/formats/netcdf/v2.0/',
+          title: 'NCEI NetCDF Templates v2.0',
+          text:  'NCEI NetCDF Templates v2.0',
+        },
+        {
+          href:  'http://data.nodc.noaa.gov/thredds/catalog/testdata/mbiddle/GOLD_STANDARD_NETCDF/catalog.html',
+          title: 'NCEI Gold Standard Example',
+          text:  'NCEI Example',
+        }
+    ]);
     // Initialize the Navbar with a Logout button
     this.views.navbar = new IOOSNavbarView({
-      el: $('#navbar-view')
+      el: $('#navbar-view'),
+      referenceUrls: referenceUrls
     });
     this.views.navbar.render();
 
--- cchecker_web/static/js/views/IOOSNavbarView.js
+++ cchecker_web/static/js/views/IOOSNavbarView.js
@@ -5,18 +5,18 @@
 
 var IOOSNavbarView = BaseView.extend({
   initialize:function(options) {
-    options = _.extend({
-      links: []
+    this.options = _.extend({
+      links: [],
+      referenceUrls: [],
     }, options);
-
-    this.links = options.links;
 
     BaseView.prototype.initialize.call(this);
   },
   templateName: "IOOSNavbar",
   render: function() {
     var self = this;
-    this.$el.html(this.template({links: this.links}));
+    this.$el.html(this.template({links: this.options.links,
+                                 referenceUrls: this.options.referenceUrls}));
     return this;
   }
 });
