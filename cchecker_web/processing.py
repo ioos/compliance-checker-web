0453cd2c0f18ea2df2e77135fb54dbd0a01aee03
Bob Fratantonio <bobfrat@gmail.com>
2017-09-17 22:05:08 -0400

    Add download text endpoint for compliance checker reports

--- cchecker_web/processing.py
+++ cchecker_web/processing.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from compliance_checker.runner import CheckSuite
+from __future__ import print_function
+from __future__ import unicode_literals
+from compliance_checker.runner import CheckSuite, ComplianceChecker
 from rq.connections import get_current_connection
-
 import base64
 import logging
 import requests
 import json
+import io
+import os
+import sys
 import subprocess
+from flask import Response
+from contextlib import contextmanager
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+# Py 3.4+ has contextlib.redirect_stdout to redirect stdout to a different
+# stream, but use this decorated function in order to redirect output in
+# previous versions
+@contextmanager
+def stdout_redirector(stream):
+    old_stdout = sys.stdout
+    sys.stdout = stream
+    try:
+        yield
+    finally:
+        sys.stdout = old_stdout
 
 
 def compliance_check(job_id, dataset, checker):
@@ -48,8 +67,20 @@ def compliance_check(job_id, dataset, checker):
         aggregates['ncdump'] = ncdump(dataset)
         buf = json.dumps(aggregates)
 
+        # Write the report to a text file for download
+        path = os.path.dirname(dataset)
+        fname = 'compliance_{}.txt'.format(job_id)
+        output_filename = os.path.join(path, fname)
+        verbose = 2
+        limit = 'strict'
+        with io.open(output_filename, 'w', encoding='utf-8') as f:
+            with stdout_redirector(f):
+                stdout_output(cs, aggregates, groups, limit, checker)
+
         redis.set('processing:job:%s' % job_id, buf, 3600)
+
         return True
+
     except Exception as e:
         logger.exception("Failed to process job")
         error_message = {
@@ -59,6 +90,33 @@ def compliance_check(job_id, dataset, checker):
         redis.set('processing:job:%s' % job_id, json.dumps(error_message), 3600)
         return False
 
+
+def stdout_output(cs, aggregates, groups, limit, checker):
+    '''
+    Calls output routine to display results in terminal, including scoring.
+    Goes to verbose function if called by user.
+
+    :param CheckSuite cs: Compliance Checker Suite
+    :param dict aggregates: Dictionary of the Compliance Checker results
+    :param list groups: List of results
+    :param int limit: The degree of strictness, 1 being the strictest, and going up from there.
+    :param str checker: The name of the compliance checker test
+    '''
+    points = aggregates['scored_points']
+    out_of = aggregates['possible_points']
+    source_name = aggregates['source_name']
+
+    # Print out the headers, (slightly customized from typical CC report)
+    print('\n')
+    print("-" * 80)
+    print('{:^80}'.format("Your dataset scored %r out of %r points" % (points, out_of)))
+    print('{:^80}'.format("during the %s check" % checker))
+    print('{:^80}'.format("Source name: %s" % source_name))
+    print("-" * 80)
+
+    # Generate the report as normal
+    cs.verbose_output_generation(groups, limit, points, out_of)
+    return groups
 
 def check_redirect(dataset, checked_urls=None):
     '''
